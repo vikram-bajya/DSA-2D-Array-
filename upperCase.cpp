ba70c819a14a7c3b72d1c7dbf5d25abcf477cda0
Vikram Bajya <vjat39915@gmail.com>
2026-06-14 20:35:03 +0530

    char arr to upper case and lower case

--- upperCase.cpp
+++ upperCase.cpp
@@ -1,40 +1,40 @@
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// // void upperCase(char word[],int size){
-// //     for(int i=0;i<size;i++){
-// //         char ch=word[i];
-// //         if(ch>='A'&&ch<='Z'){
-// //             continue;
-// //         }else{
-// //          word[i]=ch-'a'+'A';
-// //         }
-// //     }
-// // }
-
-
-// void lowerCase(char word[],int size){
-//     for(int i=0;i<size;i++){
-//         char ch=word[i];
-//         if(ch>='a'&&ch<='z'){
-//             continue;
-//         }else{
-//          word[i]=ch-'A'+'a';
-//         }
-//     }
-// }
+void upperCase(char word[],int size){
+    for(int i=0;i<size;i++){
+        char ch=word[i];
+        if(ch>='A'&&ch<='Z'){
+            continue;
+        }else{
+         word[i]=ch-'a'+'A';
+        }
+    }
+}
 
 
-// int main (){
+void lowerCase(char word[],int size){
+    for(int i=0;i<size;i++){
+        char ch=word[i];
+        if(ch>='a'&&ch<='z'){
+            continue;
+        }else{
+         word[i]=ch-'A'+'a';
+        }
+    }
+}
 
-//     char word[]="ApPle";
-//     int size=sizeof(word)/sizeof(char)-1;
+
+int main (){
+
+    char word[]="ApPle";
+    int size=sizeof(word)/sizeof(char)-1;
    
 
-//     // upperCase(word,size);
-//     lowerCase(word,size);
+    upperCase(word,size);
+    lowerCase(word,size);
 
-//     cout<<word;
+    cout<<word;
 
-//     return 0;
-// }
+    return 0;
+}
